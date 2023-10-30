5cd9579973c97a4dedc077f2857f34b9820bba14
Acevedo Ariel <AcevedoAriel@outlook.com>
2023-10-30 10:52:40 -0300

    primera configuracion de MAVEN

---
+++ comision114/src/main/java/edu/ap4t2/App.java
@@ -0,0 +1,13 @@
+package edu.ap4t2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
